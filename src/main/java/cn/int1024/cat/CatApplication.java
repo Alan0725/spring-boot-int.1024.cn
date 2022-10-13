378ede79db8d13af192e779ff80d239651b15f78
DengXiangHong <dengxianghong24@gmail.com>
2022-10-13 23:12:49 +0800

    :zap: initialize

--- src/main/java/cn/int1024/cat/CatApplication.java
+++ src/main/java/cn/int1024/cat/CatApplication.java
@@ -1,6 +1,5 @@
 package cn.int1024.cat;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2022/10/12 21:19
  * @Version: 1.0
  */
-@MapperScan({"cn.int1024.cat.mapper"})
 @SpringBootApplication
 public class CatApplication {
 
